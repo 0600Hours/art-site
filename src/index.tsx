354d44533c69188f6a9a5fdea0cff31123a196e2
Thomas' Pendley <tpendley360@gmail.com>
2023-06-02 13:18:42 -0700

    see if index nonsense fixes things

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Page from './components/Page';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import Contact from './routes/Contact';
 import Portfolio from './routes/Portfolio';
 import SingleImage from './routes/SingleImage';
